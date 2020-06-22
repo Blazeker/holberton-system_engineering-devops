0e6e307cb8275804ae24479f4c54d9352129dc38
Blazeker <1687@holbertonschool.com>
2020-06-22 16:16:00 +0000

    Signals topic

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t pid;
+    int childs = 0;
+
+    while(childs < 5)
+    {
+        pid = fork();
+        if (pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", pid);
+            childs++;
+        }
+        else
+            exit(0);
+    }
+
+    infinite_while();
+    return 0;
+}
