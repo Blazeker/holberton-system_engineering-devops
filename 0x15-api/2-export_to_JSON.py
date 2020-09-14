4b9022405e8c7861797bfb67add49fd61c1a4781
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.com>
2020-09-14 18:27:05 -0400

    pep8 fixed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     file_name = id_employees + ".json"
 
     todo_url = "https://jsonplaceholder.typicode.com/users/"\
-                + id_employees + "/todos"
+               + id_employees + "/todos"
     user_url = "https://jsonplaceholder.typicode.com/users/"
 
     url1 = requests.get(todo_url)
