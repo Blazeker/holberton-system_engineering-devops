f33fe4b132713c663c54c4ac683f94885a344f1e
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.com>
2020-09-14 18:19:31 -0400

    Syntax error

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,20 +9,19 @@ if __name__ == "__main__":
 
     id_employees = argv[1]
     list_aux = []
-    file_name = "USER_ID.json"
+    file_name = id_employees + ".json"
 
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    user_url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "https://jsonplaceholder.typicode.com/users/"
 
-    url1 = requests.get(todo_url, params={'userId': id_employees})
-    url2 = requests.get(user_url, params={'id': id_employees})
+    url1 = requests.get(todo_url + "todos?userId=" + id_employees)
+    url2 = requests.get(user_url + id_employees)
     info_user = url2.json()
-    name_employee = info_user[0].get("name")
-
+    name_employee = info_user[0].get("username")
     for i in url1.json():
         list_aux.append({"task": i.get("title"),
-                        "completed": i.get("completed"),
-                       "username": i.get(name_employee)})
+                         "completed": i.get("completed"),
+                         "username": i.get(name_employee)})
     dict_aux = {argv[1]: list_aux}
     with open(file_name, mode="w", encoding="utf-8") as file:
         file.write(json.dumps(dict_aux))
